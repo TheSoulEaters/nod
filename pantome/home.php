a32a32ad0370cd97baaae9ecc8452c6c1de94ba9
unknown <hekpiksel@gmail.com>
2024-10-30 13:08:19 +0700

    baru

---
+++ pantome/home.php
@@ -0,0 +1,38 @@
+<?php
+    // PHP code here
+
+
+
+
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/home.css">
+    <!--icon web -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+    <div>
+       <nav>
+           <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="profile/profile.php">profile</a></li>
+           </ul>
+        </nav>
+    </div>
+
+    <h1>Ini adalah halaman Home</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
